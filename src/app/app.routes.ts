d7836c662c2e0ece8f08eb2544e518a379b66e85
hgueguen <hortense.gueguen@etudiant.univ-lr.fr>
2026-01-10 11:00:14 +0100

    ajout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProjectPageComponent } from './components/ProjectPage/project-page.component';
+import { TaskDetailComponent } from './components/TaskDetail/task-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'project', component: ProjectPageComponent },
+    { path: 'task/:id', component: TaskDetailComponent },
+];
